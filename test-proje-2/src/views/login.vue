57662a8a003ef73ac068b9006c7d89813acc2204
furkan <furkan.ede1@gmail.com>
2023-05-09 13:45:36 +0300

    login-dashboard-vue

--- test-proje-2/src/views/login.vue
+++ test-proje-2/src/views/login.vue
@@ -1,28 +1,33 @@
 <template>
   <div class="login">
     <h3>Giriş yap</h3>
-    <input type="email"
+    <input
+      type="email"
       placeholder="Email"
       v-model="email"
       class="form-control"
       @blur="$v.email.$touch()"
-      :class="{'is-invalid': $v.email.$error}"
+      :class="{ 'is-invalid': $v.email.$error }"
     />
     <small v-if="$v.email.$error">
       <span v-if="!$v.email.required">Lütfen bir email adresi yazın</span>
       <span v-if="!$v.email.email">Lütfen geçerli bir email adresi yazın</span>
     </small>
 
-    <input type="password"
+    <input
+      type="password"
       placeholder="Şifre"
       v-model="password"
       class="form-control"
       @blur="$v.password.$touch()"
-      :class="{'is-invalid': $v.password.$error}"
+      :class="{ 'is-invalid': $v.password.$error }"
     />
     <small v-if="$v.password.$error">
       <span v-if="!$v.password.required">Lütfen bir Şifre yazın</span>
-      <span v-if="!$v.password.valid">Şifreniz en az bir küçük harf, bir büyük harf ve bir rakam içermelidir</span>
+      <span v-if="!$v.password.valid"
+        >Şifreniz en az bir küçük harf, bir büyük harf ve bir rakam
+        içermelidir</span
+      >
     </small>
 
     <button @click="submitData">Giriş yap</button>
@@ -31,15 +36,15 @@
 
 <script>
 import Api from "../services/api";
-import { useVuelidate } from '@vuelidate/core';
-import { required, email } from '@vuelidate/validators';
+import { useVuelidate } from "@vuelidate/core";
+import { required, email } from "@vuelidate/validators";
 
 export default {
   name: "UserLogin",
- setup() {
-  return v$; {useVuelidate() }
- },
-  
+  setup() {
+    return { v$: useVuelidate() };
+  },
+
   data() {
     return {
       email: "",
@@ -47,9 +52,8 @@ export default {
     };
   },
   methods: {
-    submitData: async function() {
+    submitData: async function () {
       try {
-        const $v = useVuelidate();
         const data = { Username: this.email, Password: this.password };
         const api = new Api();
         const response = await api.login(data);
@@ -69,14 +73,14 @@ export default {
     email: { required, email },
     password: {
       required,
-      valid: value => {
+      valid: (value) => {
         const containsUppercase = /[A-Z]/.test(value);
         const containsLowercase = /[a-z]/.test(value);
         const containsNumbercase = /[0-9]/.test(value);
         return containsUppercase && containsLowercase && containsNumbercase;
-      }
-    }
-  }
+      },
+    },
+  },
 };
 </script>
 
@@ -100,7 +104,6 @@ button {
   cursor: pointer;
   padding: 10px;
 }
-
 
 p {
   margin-top: 40px;
