b58246ba32712a14d36498aec6c2e8fb2e3739fb
furkan <furkan.ede1@gmail.com>
2023-05-11 16:42:27 +0300

    Update router.js

--- test-proje-2/src/views/src/router.js
+++ test-proje-2/src/views/src/router.js
@@ -3,6 +3,7 @@ import Home from "./views/Home.vue";
 import Login from "./views/Login.vue";
 import Register from "./views/Register.vue";
 import VueRouter from "vue-router";
+import Dashboard from "./views/Dashboard";
 
 Vue.use(VueRouter);
 
@@ -10,6 +11,11 @@ export default new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: "Dashboard",
+      name: "Dashboard",
+      component: Dashboard,
+    },
     {
       path: "/",
       name: "home",
