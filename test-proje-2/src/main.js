dad7f9e75fb4cd77f52728ae0ea0727ffc9a1d63
furkan <furkan.ede1@gmail.com>
2023-05-11 15:12:16 +0300

    ..

--- test-proje-2/src/main.js
+++ test-proje-2/src/main.js
@@ -4,10 +4,9 @@ import router from "./router";
 import ToastPlugin from "vue-toast-notification";
 import "vue-toast-notification/dist/theme-bootstrap.css";
 import store from "./views/store";
-import Vuelidate from "vuelidate";
 const app = createApp(App);
 app
-  .use(Vuelidate)
+
   .use(store)
   .use(router)
   .use(ToastPlugin, { position: "top-right" })
--- test-proje-2/src/views/login.vue
+++ test-proje-2/src/views/login.vue
@@ -1,34 +1,25 @@
 <template>
   <div class="login">
     <h3>Giriş yap</h3>
+
     <input
       type="email"
       placeholder="Email"
       v-model="email"
       class="form-control"
-      @blur="$v.email.$touch()"
-      :class="{ 'is-invalid': $v.email.$error }"
+      :class="{ 'is-invalid': v$.email.$error }"
     />
-    <small v-if="$v.email.$error">
-      <span v-if="!$v.email.required">Lütfen bir email adresi yazın</span>
-      <span v-if="!$v.email.email">Lütfen geçerli bir email adresi yazın</span>
-    </small>
+
+    <span v-if="v$.email.$error" style="color: red">
+      {{ v$.email.$errors[0].$message }}
+    </span>
 
     <input
       type="password"
       placeholder="Şifre"
       v-model="password"
       class="form-control"
-      @blur="$v.password.$touch()"
-      :class="{ 'is-invalid': $v.password.$error }"
     />
-    <small v-if="$v.password.$error">
-      <span v-if="!$v.password.required">Lütfen bir Şifre yazın</span>
-      <span v-if="!$v.password.valid"
-        >Şifreniz en az bir küçük harf, bir büyük harf ve bir rakam
-        içermelidir</span
-      >
-    </small>
 
     <button @click="submitData">Giriş yap</button>
   </div>
@@ -36,11 +27,14 @@
 
 <script>
 import Api from "../services/api";
+
 import { useVuelidate } from "@vuelidate/core";
+
 import { required, email } from "@vuelidate/validators";
 
 export default {
   name: "UserLogin",
+
   setup() {
     return { v$: useVuelidate() };
   },
@@ -48,38 +42,47 @@ export default {
   data() {
     return {
       email: "",
+
       password: "",
     };
   },
+
   methods: {
     submitData: async function () {
       try {
-        const data = { Username: this.email, Password: this.password };
-        const api = new Api();
-        const response = await api.login(data);
-        console.log(response.data);
-        if (response.data.IsSucceed) {
-          this.$toast.success("Giriş başarılı.");
-          this.$router.push("/user-dashboard");
+        console.log(this);
+
+        this.v$.$validate();
+
+        if (!this.v$.$error) {
+          const data = { Username: this.email, Password: this.password };
+
+          const api = new Api();
+
+          const response = await api.login(data);
+
+          console.log(response.data);
+
+          if (response.data.IsSucceed) {
+            this.$toast.success("Giriş başarılı.");
+
+            this.$router.push("/user-dashboard");
+          } else {
+            this.$toast.error("Giriş başarısız.");
+          }
         } else {
-          this.$toast.error("Giriş başarısız.");
+          this.$toast.error("failed valid");
         }
       } catch (error) {
         console.error(error);
       }
     },
   },
-  validations: {
-    email: { required, email },
-    password: {
-      required,
-      valid: (value) => {
-        const containsUppercase = /[A-Z]/.test(value);
-        const containsLowercase = /[a-z]/.test(value);
-        const containsNumbercase = /[0-9]/.test(value);
-        return containsUppercase && containsLowercase && containsNumbercase;
-      },
-    },
+
+  validations() {
+    return {
+      email: { required, email }, // Matches this.firstName
+    };
   },
 };
 </script>
@@ -87,31 +90,45 @@ export default {
 <style scoped>
 .login {
   margin-top: 40px;
+
   display: flex;
+
   flex-direction: column;
+
   align-items: center;
+}
+
+.is-invalid {
+  border: 1px solid red;
 }
 
 input {
   margin: 10px 0;
+
   width: 25%;
+
   padding: 10px;
 }
 
 button {
   margin-top: 10px;
+
   width: 15%;
+
   cursor: pointer;
+
   padding: 10px;
 }
 
 p {
   margin-top: 40px;
+
   font-size: 14px;
 }
 
 p a {
   text-decoration: underline;
+
   cursor: pointer;
 }
 </style>
